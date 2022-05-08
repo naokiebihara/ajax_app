5c216bfc33c40e29a008fbec7287e5076e4d8762
naokiebihara <102887633+naokiebihara@users.noreply.github.com>
2022-05-08 16:01:28 +0900

    コントローラーにリクエストを送信する

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,14 +3,15 @@ class PostsController < ApplicationController
   def index
     @posts = Post.order(id: "DESC")
   end
-
-    # コメントアウトする
-    # def new
-    # end
-
+ 
+   # コメントアウトする
+   # def new
+   # end
+ 
   def create
-    Post.create(content: paramas[:content])
-    redirect_to action: :index # 追記する
+    Post.create(content: params[:content])
+    redirect_to action: :index 
   end
-
-end
+ 
+ end
+ 
